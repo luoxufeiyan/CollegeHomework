f7bdefbc3539ab6685eaa3f6eeb6048cffb768f6
LXFY <luoxufeiyan@gmail.com>
2015-06-03 16:26:10 +0800

    Add

--- 数据结构/约瑟夫环/MyJosephusProblem.cpp
+++ 数据结构/约瑟夫环/MyJosephusProblem.cpp
@@ -1,24 +1,26 @@
 // My accepted code of Q2
 #include <stdlib.h>
-#include <iostream>
+#include <iostream>//C++
+//#include<stdio.c>
 #include <stdarg.h>
 using namespace std;
 
+
 typedef struct _LinkNode //开链表
 {
-	int value;
+	int value;//值
 	struct _LinkNode* next;
-}LinkNode, *LinkNodePtr;
+}LinkNode, *LinkNodePtr;//Ptr==Pointer
 
-LinkNodePtr createCycle(int total)
+LinkNodePtr createCycle(int total)//创建循环
 {
-	int index = 1;
+	int index = 1;//初始化目录为1
 	LinkNodePtr head = NULL, curr = NULL, prev = NULL;//链表初始化
 	head = (LinkNodePtr)malloc(sizeof(LinkNode));
 	head->value = index;
-	prev = head;
+	prev = head;//循环
 
-	while (--total > 0)//判断满链表？
+	while (--total > 0)// total--
 	{
 		curr = (LinkNodePtr)malloc(sizeof(LinkNode));
 		curr->value = ++index;
@@ -32,12 +34,13 @@ LinkNodePtr createCycle(int total)
 void run(int total, int tag)
 {
 	LinkNodePtr node = createCycle(total);
-	LinkNodePtr prev = NULL;
+	//total;
+	//LinkNodePtr prev = NULL;//初始化
 	int start = 1;
 	int index = start;
 	while (node && node->next)
 	{
-		if (index == tag)
+		if (index == tag)//找到了某小朋友
 		{
 			printf("\n%d", node->value);
 			if (tag == start)
@@ -65,15 +68,17 @@ void run(int total, int tag)
 int main(int argc, char* argv[])
 {
 	int peo;
+	int i=1;
 	while (1)
 	{
-		cout << "请输入初始序号，退出按1么么哒~" << endl;
-		cin >> peo;
+		cout << "请输入初始序号，退出按1" << endl;
+		//cin >> peo;
+		scanf("%d",&peo);
 		if (peo == 1)
 			break;
 		else if (peo < 1)
 		{
-			cout << "输入不合法，必须要输入大于一的正整数呀~" << endl;
+			cout << "输入不合法，必须要输入大于一的正整数" << endl;
 			continue;
 		}
 		else
