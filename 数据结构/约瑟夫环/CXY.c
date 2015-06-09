ec5e86477ee9751a4c3f7267c305d0fbcacedc81
LXFY <luoxufeiyan@gmail.com>
2015-06-09 20:58:00 +0800

    C语言版本

---
+++ 数据结构/约瑟夫环/CXY.c
@@ -0,0 +1,90 @@
+//C version
+#include <stdlib.h>
+//#include <iostream>//C++
+#include<stdio.c>
+#include <stdarg.h>
+//using namespace std;
+
+
+typedef struct _LinkNode //开链表
+{
+	int value;//值
+	struct _LinkNode* next;
+}LinkNode, *LinkNodePtr;//Ptr==Pointer
+
+LinkNodePtr createCycle(int total)//创建循环
+{
+	int index = 1;//初始化目录为1
+	LinkNodePtr head = NULL, curr = NULL, prev = NULL;//链表初始化
+	head = (LinkNodePtr)malloc(sizeof(LinkNode));
+	head->value = index;
+	prev = head;//循环
+
+	while (--total > 0)// total
+	{
+		curr = (LinkNodePtr)malloc(sizeof(LinkNode));
+		curr->value = ++index;
+		prev->next = curr;
+		prev = curr;
+	}
+	curr->next = head;
+	return head;
+}
+
+void run(int total, int tag)
+{
+	LinkNodePtr node = createCycle(total);
+	//total;
+	LinkNodePtr prev = NULL;//初始化
+	int start = 1;
+	int index = start;
+	while (node && node->next)
+	{
+		if (index == tag)//找到了某小朋友
+		{
+			printf("\n%d", node->value);
+			if (tag == start)
+			{
+				prev = node->next;
+				node->next = NULL;
+				node = prev;
+			}
+			else
+			{
+				prev->next = node->next;
+				node->next = NULL;
+				node = prev->next;
+			}
+			index = start;
+		}
+		else
+		{
+			prev = node;
+			node = node->next;
+			index++;
+		}
+	}
+}
+int main(int argc, char* argv[])
+{
+	int peo;
+	int i=1;
+	while (1)
+	{
+		printf("请输入初始序号，退出按1。\n");
+		scanf("%d",&peo);
+		if (peo == 1)
+			break;
+		else if (peo < 1)
+		{
+			printf("输入不合法，必须输入大于一的正整数");
+			continue;
+		}
+		else
+		{
+			run(peo, 999999);
+			printf("\n");
+		}
+	}
+	return 0;
+}
