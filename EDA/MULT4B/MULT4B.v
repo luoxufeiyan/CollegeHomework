ec842b5adf030421f21cd28df073aa5811042a7c
LXFY <luoxufeiyan@gmail.com>
2016-04-06 17:34:26 +0800

    add MULT4B

---
+++ EDA/MULT4B/MULT4B.v
@@ -0,0 +1,13 @@
+module MULT4B(R,A,B);
+	parameter S=4;
+	output[2*S:1] R;
+	input[S:1] A,B;
+	reg[2*S:1] R;
+	integer i;
+	always@(A or B)
+	begin
+	R=0;
+	for(i=1;i<=S;i=i+1)
+	if(B[i]) R=R+(A<<(i-1));
+	end
+endmodule
